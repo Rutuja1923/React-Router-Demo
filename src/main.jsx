c8bb5c279e00aa59ad28feecce5925b870f99083
Rutuja1923 <rutujajirage19@gmail.com>
2025-04-05 15:42:03 +0530

    Another way to render elements using Route tag

--- src/main.jsx
+++ src/main.jsx
@@ -2,32 +2,28 @@ import "./index.css";
 import ReactDOM from 'react-dom/client'
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route } from "react-router-dom";
 import Layout from "./Layout";
 import Home from "./Components/Home/Home";
 import About from "./Components/About/About";
 import Contact from "./Components/Contact/Contact";
+import User from "./Components/User/User";
+import Github from "./Components/Github/Github";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout />,
-    children: [
-      {
-        path: "",
-        element: <Home />,
-      },
-      {
-        path: "about",
-        element: <About />,
-      },
-      {
-        path: "contact",
-        element: <Contact />,
-      },
-    ],
-  },
-]);
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout />}>
+      <Route path='' element={<Home />} />  
+      <Route path='/about' element={<About />} />
+      <Route path='/contact' element={<Contact />} />  
+      <Route path='/user/:id' element={<User />} />
+      <Route 
+        path='/github' 
+        element={<Github />} 
+      />
+    </Route>
+  )
+);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>

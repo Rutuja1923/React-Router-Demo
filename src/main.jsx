a924ac54a6f921843d6c83021ad9153ed9de6c09
Rutuja1923 <rutujajirage19@gmail.com>
2025-04-05 16:08:30 +0530

    using Loader for API call optimization

--- src/main.jsx
+++ src/main.jsx
@@ -1,26 +1,28 @@
 import "./index.css";
-import ReactDOM from 'react-dom/client'
+import ReactDOM from "react-dom/client";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route } from "react-router-dom";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
 import Layout from "./Layout";
 import Home from "./Components/Home/Home";
 import About from "./Components/About/About";
 import Contact from "./Components/Contact/Contact";
 import User from "./Components/User/User";
-import Github from "./Components/Github/Github";
+import Github, {githubInfoLoader } from "./Components/Github/Github";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path='/' element={<Layout />}>
-      <Route path='' element={<Home />} />  
-      <Route path='/about' element={<About />} />
-      <Route path='/contact' element={<Contact />} />  
-      <Route path='/user/:id' element={<User />} />
-      <Route 
-        path='/github' 
-        element={<Github />} 
-      />
+    <Route path="/" element={<Layout />}>
+      <Route path="" element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="contact" element={<Contact />} />
+      <Route path="user/:id" element={<User />} />
+      <Route loader={githubInfoLoader} path="github" element={<Github />} />
     </Route>
   )
 );
